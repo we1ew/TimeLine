11bde303a771b199e5792ae289b7513a86141d21
we1ew <we1ew@naver.com>
2020-02-23 19:39:24 +0900

    Test, write simple comment

--- app/src/main/java/CodingforSimpleLife/pdsu/timeline/MainActivity.kt
+++ app/src/main/java/CodingforSimpleLife/pdsu/timeline/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // DB를 통해서 오늘 날짜 타임라인 정보가 있으면 불러옴
     }
 }
